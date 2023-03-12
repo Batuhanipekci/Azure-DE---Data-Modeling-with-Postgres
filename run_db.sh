4a9c560bd3b19f01d65a9fd94d3e1f2edb28b405
Batuhan Ipekci <batuhanipekci@hotmail.com>
2023-03-12 12:39:42 +0100

    Setup database'

---
+++ run_db.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --name postgres_udacity -v volume_postgres:/data/db -h 127.0.0.1 -p 5432:5432 -e POSTGRES_USER=student -e POSTGRES_PASSWORD=student -e POSTGRES_DB=studentdb -d postgres:latest
